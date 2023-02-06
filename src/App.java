2caa068e0a69af4d416200903157bcfe980aa13b
aajackson2009@yahoo.com <aajackson2009@yahoo.com>
2023-02-06 02:17:35 -0500

    Incorporatedthe controller project

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import com.sun.net.httpserver.HttpServer;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Creating as well as starting the backend server, to enable it to start receiving and responding to requests.
+        HttpServer server = HttpServer.create();
+
+
+
+
+
+    }
+}
